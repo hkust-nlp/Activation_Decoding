70285a3d56cf78bcdff3b084720d277b7fd574c2
Shiqi Chen <shiqichen@jxcpu1.cse.ust.hk>
2024-02-17 13:06:13 +0800

    update

---
+++ scripts/run_mctqa.sh
@@ -0,0 +1,45 @@
+export TRANSFORMERS_CACHE='/ssddata/model_hub'
+export CUDA_VISIBLE_DEVICES="0"
+
+model=daryl149/llama-2-7b-chat-hf
+# model could also be "daryl149/llama-2-13b-chat-hf" or "daryl149/llama-2-70b-chat-hf" 
+model_name="${model#*/}"
+printf "Model name: $model_name\n"
+
+####BASELINE####
+mode="baseline"
+output_path="../res/res_mctqa/${model_name}_${mode}.json" 
+# python ../eval_tqamc.py --model-name $model --decoding_mode $mode --data-path ./data --output-path $output_path --num-gpus 1  
+
+####DOLA####
+early_exit_layers="0,2,4,6,8,10,12,14,32"
+if [ "$model_name" == "llama-2-13b-chat-hf" ]; then
+    early_exit_layers="0,2,4,6,8,10,12,14,16,18,40"
+elif  [ "$model_name" == "llama-2-70b-chat-hf" ]; then
+    early_exit_layers="0,2,4,6,8,10,12,14,16,18,80"
+fi
+mode="dola"
+output_path="../res/res_mctqa/${model_name}_${mode}.json" 
+# python ../eval_tqamc.py --model-name $model --decoding_mode $mode --early-exit-layers $early_exit_layers --data-path ./data --output-path $output_path --num-gpus 1  
+
+
+####ACTIVATION####
+alpha="0.5"
+info_layer="26"
+mode="activation"
+decoding_strategy="entropy"
+output_path="../res/res_mctqa/${model_name}_${mode}_${decoding_strategy}_${alpha}${info_layer}.json"
+python ../eval_tqamc.py --model-name $model --data-path ./data --output-path $output_path --num-gpus 1 --do-rating --gpt3-config ../gpt3.config.json \
+--decoding_mode $mode --alpha $alpha --info_layer $info_layer --decoding_strategy $decoding_strategy 
+
+
+####ACTIVATION_DOLA####
+alpha="0.5"
+info_layer="26"
+mode="activation_dola"
+decoding_strategy="entropy"
+output_path="../res/res_mctqa/${model_name}_${mode}_${decoding_strategy}_${alpha}${info_layer}.json"
+# python ../eval_tqamc.py --model-name $model --early-exit-layers $early_exit_layers --data-path ./data --output-path $output_path --num-gpus 1 --do-rating --gpt3-config ../gpt3.config.json \
+# --decoding_mode $mode --alpha $alpha --info_layer $info_layer --decoding_strategy $decoding_strategy 
+
+
---
+++ scripts/run_qa.sh
@@ -0,0 +1,50 @@
+export TRANSFORMERS_CACHE='/ssddata/model_hub'
+export HF_DATASETS_CACHE='/ssddata/model_hub'
+export PYTORCH_KERNEL_CACHE_PATH='/ssddata/shiqi/error_tracing_4.31.0'
+export CUDA_VISIBLE_DEVICES="3" 
+
+dataset='hotpotqa'
+# Here the dataset could also be "natural_questions" or "triviaqa"
+
+data_path=../scripts/data/hpqa
+# For "natural_questions", path is "../scripts/data/nq". For "triviaqa", path is "../scripts/data/trqa". 
+
+model=daryl149/llama-2-7b-chat-hf
+# model could also be "daryl149/llama-2-13b-chat-hf" or "daryl149/llama-2-70b-chat-hf" 
+model_name="${model#*/}"
+printf "Model name: $model_name\n"
+
+##### BASELINE #####
+decoding_mode="baseline"
+# decoding_mode could be "baseline", "dola", "activation","activation_dola"
+output_path="../res/res_hpqa/${model_name}/${model_name}_${decoding_mode}.json"
+python ../eval_knowledge_qa.py --model-name $model --dataset_name $dataset --decoding_mode $decoding_mode  --output-path $output_path --num-gpus 1 --do-rating 
+
+##### DOLA #####
+early_exit_layers="0,2,4,6,8,10,12,14,32"
+if [ "$model_name" == "Llama-2-13b-chat-hf" ]; then
+    early_exit_layers="0,2,4,6,8,10,12,14,16,18,40"
+elif [ "$model_name" == "llama-2-70b-chat-hf" ]; then
+    early_exit_layers="0,2,4,6,8,10,12,14,16,18,80"
+fi
+decoding_mode="dola"
+output_path="../res/res_hpqa/${model_name}/${model_name}_${decoding_mode}.json"
+python ../eval_knowledge_qa.py --model-name $model --dataset_name $dataset --decoding_mode $decoding_mode --early_exit_layers $early_exit_layers  --output-path $output_path --num-gpus 1 --do-rating
+
+##### ACTIVATION #####
+decoding_mode="activation"
+alpha="1"
+info_layer="32"
+decoding_strategy="entropy"
+# decoding_strategy can be "entropy" or "single_entropy"
+output_path="../res/res_hpqa/${model_name}/${model_name}_${decoding_mode}_${alpha}_${info_layer}.json"
+python ../eval_knowledge_qa.py --model-name $model --dataset_name $dataset --decoding_mode $decoding_mode  --alpha $alpha --info_layer $info_layer --decoding_strategy $decoding_strategy --output-path $output_path --num-gpus 1 --do-rating --data_path $data_path 
+
+
+##### ACTIVATION_DOLA #####
+decoding_mode="activation"
+alpha="1"
+info_layer="32"
+decoding_strategy="entropy"
+output_path="../res/res_hpqa/${model_name}/${model_name}_${decoding_mode}_${alpha}_${info_layer}.json"
+python ../eval_knowledge_qa.py --model-name $model --dataset_name $dataset --decoding_mode $decoding_mode --early_exit_layers $early_exit_layers  --alpha $alpha --info_layer $info_layer --decoding_strategy $decoding_strategy --output-path $output_path --num-gpus 1 --do-rating --data_path $data_path
